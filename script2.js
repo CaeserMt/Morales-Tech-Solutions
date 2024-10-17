5229417633b8fe6797e4706a8b307e153b87ae3c
CaeserMt <cm544713@gmail.com>
2024-10-16 19:48:03 -0500

    Cesar

--- script2.js
+++ script2.js
@@ -16,3 +16,39 @@ function nextSlide() {
 }
 
 setInterval(nextSlide, 3000); // Cambia de imagen cada 3 segundos
+
+// Abre el lightbox
+function openLightbox(index) {
+    var lightbox = document.getElementById("lightbox");
+    var images = document.querySelectorAll(".gallery-slide img");
+    var fullImage = document.getElementById("fullImage");
+    var caption = document.getElementById("caption");
+
+    lightbox.style.display = "block";
+    currentSlide = index;
+    fullImage.src = images[currentSlide].src;
+    caption.innerHTML = images[currentSlide].alt;
+}
+
+// Cierra el lightbox
+document.querySelector(".close").onclick = function() {
+    document.getElementById("lightbox").style.display = "none";
+};
+
+// Cambia de imagen
+var currentSlide = 0;
+function changeSlide(n) {
+    var images = document.querySelectorAll(".gallery-slide img");
+    currentSlide = (currentSlide + n + images.length) % images.length;
+    var fullImage = document.getElementById("fullImage");
+    var caption = document.getElementById("caption");
+    fullImage.src = images[currentSlide].src;
+    caption.innerHTML = images[currentSlide].alt;
+}
+
+// Cuando se hace clic en una imagen, abrir en pantalla completa
+document.querySelectorAll(".gallery-slide img").forEach(function(image, index) {
+    image.onclick = function() {
+        openLightbox(index);
+    };
+});
